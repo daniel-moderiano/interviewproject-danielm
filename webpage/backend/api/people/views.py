43891f4aefcd42223e651b682e8527b05e72ed80
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-03-11 16:24:13 +1030

    Create GET/POST view
    - Start with an explicit function based view
    - Add handlers for GET and POST commands, the only two required

--- webpage/backend/api/people/views.py
+++ webpage/backend/api/people/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from people.models import Person
+from people.serializers import PersonSerializer
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def person_list(request):
+    """
+    List all people, or create a new person.
+    """
+    if request.method == 'GET':
+        snippets = Person.objects.all()
+        serializer = PersonSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = PersonSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
