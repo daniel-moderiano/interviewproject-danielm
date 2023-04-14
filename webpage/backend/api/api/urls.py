8377cfbe6d85622170086fbb85bc43353dfce198
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-04-14 10:15:13 +0930

    Move react rendering and routes into separated django app

--- webpage/backend/api/api/urls.py
+++ webpage/backend/api/api/urls.py
@@ -1,11 +1,6 @@
-from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import render
-
-def render_react(request):
-    return render(request, "index.html")
 
 urlpatterns = [
     path('api/', include('people.urls')),
-    path('', render_react),
+    path('', include('react.urls')),
 ]
