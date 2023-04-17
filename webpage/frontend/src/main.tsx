21a1092f13066810e2f26c1074ee9e43bb38390c
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-04-17 15:19:22 +0930

    Downgrade all relevant deps

--- webpage/frontend/src/main.tsx
+++ webpage/frontend/src/main.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+const rootElement = document.getElementById("root") as HTMLElement;
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  rootElement
 );
